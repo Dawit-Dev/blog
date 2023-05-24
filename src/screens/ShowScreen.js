143a23ff6e401a2686182cfd5e0cc8a8d7f1cb7f
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-05-24 16:23:39 +0100

    Retrieved a single post

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Context } from "../context/BlogContext";
+import { useRoute } from "@react-navigation/native";
 
 const ShowScreen = () => {
+  const { state } = useContext(Context);
+  const route = useRoute();
+  const { id } = route.params;
+
+  const blogPost = state.find((blogPost) => blogPost.id === id);
+
   return (
     <View>
-      <Text>Show Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default ShowScreen
+export default ShowScreen;
