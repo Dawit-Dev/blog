06842d7f411c851d43c28ecb3e23eaae1c5f3895
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-05-24 12:20:42 +0100

    Added a bit of styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
-import {Context} from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const {state, addBlogPost} = useContext(Context);
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPosts) => blogPosts.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.text}>{item.title}</Text>
+              <Feather style={styles.icone} name="trash-2" size={24} color="black" />
+            </View>
+          );
         }}
       />
     </View>
@@ -20,14 +25,28 @@ const IndexScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+  // container: {
+  //   flex: 1,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // },
   text: {
-    fontSize: 16,
+    fontSize: 21,
   },
+  icone: {
+    fontSize: 33,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    // marginBottom: 6
+    padding: 12,
+    border: 2,
+    borderRadius: 12,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: 'gray'
+  }
 });
 
 export default IndexScreen;
