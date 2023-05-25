a38a28b3b82e02c7d467f71994f1c9d73e123e98
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-05-25 18:18:49 +0100

    "Implemented edit functionality for edit blog

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,35 +1,37 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
-const BlogPostForm = ({ onSubmit }) => {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+const BlogPostForm = ({ onSubmit, initialValues }) => {
+    const [title, setTitle] = useState(initialValues.title);
+    const [content, setContent] = useState(initialValues.content);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button
-              title="Save Blog Post"
-              onPress={() => onSubmit(title, content)}
-        // onPress={() => {
-        //   addBlogPost(title, content, () => {
-        //     navigation.navigate("Index");
-        //   });
-        // }}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.label}>Enter Title:</Text>
+            <TextInput
+                style={styles.input}
+                value={title}
+                onChangeText={(text) => setTitle(text)}
+            />
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput
+                style={styles.input}
+                value={content}
+                onChangeText={(text) => setContent(text)}
+            />
+            <Button
+                title="Save Blog Post"
+                onPress={() => onSubmit(title, content)}
+            />
+        </View>
+    );
+};
+
+BlogPostForm.defaultProps = {
+    initialValues: {
+        title: '',
+        content: ''
+    }
 }
 
 const styles = StyleSheet.create({
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,20 +1,26 @@
-import React, { useContext } from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { Context } from '../context/BlogContext';
-import BlogPostForm from '../components/BlogPostForm';
-
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = () => {
-    const route = useRoute();
-    const { id } = route.params;
-    const { state } = useContext(Context);
+  const route = useRoute();
+  const { id } = route.params;
+  const { state, editBlogPost } = useContext(Context);
 
-    const blogPost = state.find(blogPost => blogPost.id === id);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
-  return <BlogPostForm />
-}
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content)
+      }}
+    />
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default EditScreen
+export default EditScreen;
