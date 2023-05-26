d52ed7e1c3b139d76dff3a024a8695b13e0ff116
Dawit-Dev <100426510+Dawit-Dev@users.noreply.github.com>
2023-05-26 13:35:15 +0100

    Implemented deleting a post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,15 +11,15 @@ const blogReducer = (state, action) => {
       });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
+    // case "add_blogpost":
+    //   return [
+    //     ...state,
+    //     {
+    //       id: Math.floor(Math.random() * 99999),
+    //       title: action.payload.title,
+    //       content: action.payload.content,
+    //     },
+    //   ];
     default:
       return state;
   }
@@ -36,7 +36,6 @@ const getBlogPosts = dispatch => {
 const addBlogPost = (dispatch) => {
   return async (title, content, callBack) => {
     await jsonServer.post('/blogposts', { title, content })
-    // dispatch({ type: "add_blogpost", payload: { title, content } });
     if (callBack) {
       callBack();
     }
@@ -44,7 +43,8 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`)
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
